af48d6f950481fd952c2f5c8e714de3863b6efc3
Milagros Paoletti <40247040+miliPaoletti@users.noreply.github.com>
2022-12-20 14:56:48 +0100

    feat: make table scrollable (#28)

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,8 @@
-import Snackbars from "./notification/Snackbars";
-
 const Container = ({ children }: { children: JSX.Element }) => {
   return (
     <div className="bg-black grid min-h-screen grid-template-32">
       <div className='w-full h-full bg-[url("/stars.svg")] bg-repeat bg-auto bg-center px-14 font-[GreyCliff] overflow-auto'>
         {children}
-        <Snackbars />
       </div>
     </div>
   );
--- src/components/asset/table/AssetsTable.tsx
+++ src/components/asset/table/AssetsTable.tsx
@@ -73,150 +73,152 @@ const AssetsTable = () => {
         onChange={() => setHideBalance(!hideZeroBalance)}
         checked={hideZeroBalance}
       />
-      <table className="text-white w-full font-[IBM]">
-        <thead className="uppercase ">
-          <tr>
-            <th className="text-left px-8 py-4">Asset</th>
-            <th className="text-left">IBC Balance</th>
-            <th className="text-left">ERC-20 Balance</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody className="">
-          {isLoading && (
-            <MessageTable>
-              <>
-                <span className="loader"></span>
-                <p>Loading...</p>
-              </>
-            </MessageTable>
-          )}
+      <div className="mt-10 overflow-y-auto max-h-full md:max-h-[70vh]">
+        <table className="text-white w-full font-[IBM]">
+          <thead className="uppercase ">
+            <tr>
+              <th className="text-left px-8 py-4 min-w-[350px]">Asset</th>
+              <th className="text-left min-w-[200px]">IBC Balance</th>
+              <th className="text-left min-w-[200px]">ERC-20 Balance</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {isLoading && (
+              <MessageTable>
+                <>
+                  <span className="loader"></span>
+                  <p>Loading...</p>
+                </>
+              </MessageTable>
+            )}
 
-          {error && (
-            <MessageTable>
-              <>
-                {/* add exclamation icon */}
-                <p>Request failed</p>
-              </>
-            </MessageTable>
-          )}
+            {error && (
+              <MessageTable>
+                <>
+                  {/* add exclamation icon */}
+                  <p>Request failed</p>
+                </>
+              </MessageTable>
+            )}
 
-          {tableData?.length === 0 && (
-            <MessageTable>
-              <>
-                {/* add exclamation icon */}
-                <p>No results </p>
-              </>
-            </MessageTable>
-          )}
-          {tableData?.map((item: TableData, index: number) => {
-            return (
-              <tr className="" key={index}>
-                <td>
-                  <div className="flex items-center space-x-5">
-                    <Image
-                      src={`/tokens/${item.symbol.toLocaleLowerCase()}.png`}
-                      alt={item.symbol}
-                      width={35}
-                      height={35}
-                    />
-                    <div className="flex flex-col items-start ">
-                      <span className="font-bold">{item.symbol}</span>
+            {tableData?.length === 0 && (
+              <MessageTable>
+                <>
+                  {/* add exclamation icon */}
+                  <p>No results </p>
+                </>
+              </MessageTable>
+            )}
+            {tableData?.map((item: TableData, index: number) => {
+              return (
+                <tr className="" key={index}>
+                  <td>
+                    <div className="flex items-center space-x-5">
+                      <Image
+                        src={`/tokens/${item.symbol.toLocaleLowerCase()}.png`}
+                        alt={item.symbol}
+                        width={35}
+                        height={35}
+                      />
+                      <div className="flex flex-col items-start ">
+                        <span className="font-bold">{item.symbol}</span>
+                        <span className="text-sm text-darkGray5">
+                          {item.name}
+                        </span>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="flex flex-col items-start uppercase">
+                      <span className="font-bold">
+                        {convertAndFormat(item.cosmosBalance, item.decimals)}
+                      </span>
                       <span className="text-sm text-darkGray5">
-                        {item.name}
+                        {/*TODO: get value from backend  */}$
+                        {convertAndFormat(item.cosmosBalance, item.decimals)}
                       </span>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  <div className="flex flex-col items-start uppercase">
-                    <span className="font-bold">
-                      {convertAndFormat(item.cosmosBalance, item.decimals)}
-                    </span>
-                    <span className="text-sm text-darkGray5">
-                      {/*TODO: get value from backend  */}$
-                      {convertAndFormat(item.cosmosBalance, item.decimals)}
-                    </span>
-                  </div>
-                </td>
-                <td>
-                  <div className="flex flex-col items-start uppercase">
-                    <span className="font-bold">
-                      {convertAndFormat(item.erc20Balance, item.decimals)}
-                      {item.symbol.toUpperCase() === "EVMOS" ? " WEVMOS" : ""}
-                    </span>
-                    <span className="text-sm text-darkGray5">
-                      {/*TODO: get value from backend  */}
-                      {convertAndFormat(item.erc20Balance, item.decimals)}
-                    </span>
-                  </div>
-                </td>
-                <td>
-                  <div className="space-x-3 flex justify-center">
-                    <Button
-                      onClick={() => {
-                        setShow(true);
-                        setModalValues({
-                          token: item.symbol,
-                          address: address,
-                          amount: item.cosmosBalance,
-                          title: "Deposit",
-                          network: "EVMOS",
-                          decimals: item?.decimals,
-                          feeDenom: "aevmos",
-                          pubkey: value.evmosPubkey,
-                          fee: BigNumber.from("1"),
-                          erc20Balance: item.erc20Balance,
-                        });
-                      }}
-                    >
-                      <span>Deposit</span>
-                    </Button>{" "}
-                    <Button
-                      onClick={() => {
-                        setShow(true);
-                        setModalValues({
-                          token: item.symbol,
-                          address: address,
-                          amount: item?.cosmosBalance,
-                          decimals: item?.decimals,
-                          fee: BigNumber.from("1"),
-                          feeDenom: "aevmos",
-                          title: "Withdraw",
-                          network: "EVMOS",
-                          pubkey: value.evmosPubkey,
-                          erc20Balance: item.erc20Balance,
-                        });
-                      }}
-                    >
-                      <span>Withdraw</span>
-                    </Button>
-                    <Button
-                      onClick={() => {
-                        setShow(true);
-                        setModalValues({
-                          token: item.symbol,
-                          address: address,
-                          amount: item.cosmosBalance,
-                          decimals: item?.decimals,
-                          feeDenom: "aevmos",
-                          title: "Convert",
-                          network: "EVMOS",
-                          pubkey: value.evmosPubkey,
-                          fee: BigNumber.from("1"),
-                          erc20Balance: item.erc20Balance,
-                        });
-                      }}
-                    >
-                      <span>Convert</span>
-                    </Button>
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                  </td>
+                  <td>
+                    <div className="flex flex-col items-start uppercase">
+                      <span className="font-bold">
+                        {convertAndFormat(item.erc20Balance, item.decimals)}
+                        {item.symbol.toUpperCase() === "EVMOS" ? " WEVMOS" : ""}
+                      </span>
+                      <span className="text-sm text-darkGray5">
+                        {/*TODO: get value from backend  */}
+                        {convertAndFormat(item.erc20Balance, item.decimals)}
+                      </span>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="space-x-3 flex justify-center">
+                      <Button
+                        onClick={() => {
+                          setShow(true);
+                          setModalValues({
+                            token: item.symbol,
+                            address: address,
+                            amount: item.cosmosBalance,
+                            title: "Deposit",
+                            network: "EVMOS",
+                            decimals: item?.decimals,
+                            feeDenom: "aevmos",
+                            pubkey: value.evmosPubkey,
+                            fee: BigNumber.from("1"),
+                            erc20Balance: item.erc20Balance,
+                          });
+                        }}
+                      >
+                        <span>Deposit</span>
+                      </Button>{" "}
+                      <Button
+                        onClick={() => {
+                          setShow(true);
+                          setModalValues({
+                            token: item.symbol,
+                            address: address,
+                            amount: item?.cosmosBalance,
+                            decimals: item?.decimals,
+                            fee: BigNumber.from("1"),
+                            feeDenom: "aevmos",
+                            title: "Withdraw",
+                            network: "EVMOS",
+                            pubkey: value.evmosPubkey,
+                            erc20Balance: item.erc20Balance,
+                          });
+                        }}
+                      >
+                        <span>Withdraw</span>
+                      </Button>
+                      <Button
+                        onClick={() => {
+                          setShow(true);
+                          setModalValues({
+                            token: item.symbol,
+                            address: address,
+                            amount: item.cosmosBalance,
+                            decimals: item?.decimals,
+                            feeDenom: "aevmos",
+                            title: "Convert",
+                            network: "EVMOS",
+                            pubkey: value.evmosPubkey,
+                            fee: BigNumber.from("1"),
+                            erc20Balance: item.erc20Balance,
+                          });
+                        }}
+                      >
+                        <span>Convert</span>
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
       <ModalAsset show={show} modalValues={modalValues} close={close} />
     </>
   );
